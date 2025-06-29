c5b583abc2a2ca5f7420ccf43896980bb66cf6c5
ivz <96792556+ivwallz@users.noreply.github.com>
2025-06-29 19:48:15 -0300

    Update EntityDeath.java

--- src/main/java/me/rrs/headdrop/listener/EntityDeath.java
+++ src/main/java/me/rrs/headdrop/listener/EntityDeath.java
@@ -477,7 +477,7 @@ public class EntityDeath implements Listener {
             entityActions.put(EntityType.WOLF, event -> handleEntityDrop(event, "WOLF",
                     () -> {
                         Wolf wolf = (Wolf) event.getEntity();
-                        String variant = wolf.getVariant().toString().toUpperCase().replace("MINECRAFT:", "");
+                        String variant = wolf.getVariant().getKey().getKey().toUpperCase();
                         switch (variant) {
                             case "ASHEN" -> {
                                 if (wolf.isAngry()) {
@@ -579,7 +579,7 @@ public class EntityDeath implements Listener {
                     () -> {
                         try {
                             Chicken chicken = (Chicken) event.getEntity();
-                            return switch (chicken.getVariant().toString()) {
+                            return switch (chicken.getVariant().getKey().getKey().toUpperCase()) {
                                 case "COLD" -> EntityHead.CHICKEN_COLD.getSkull();
                                 case "WARM" -> EntityHead.CHICKEN_WARM.getSkull();
                                 case "TEMPERATE" -> EntityHead.CHICKEN_TEMPERATE.getSkull();
@@ -612,7 +612,7 @@ public class EntityDeath implements Listener {
                     () -> {
                         try {
                             Pig pig = (Pig) event.getEntity();
-                            return switch (pig.getVariant().toString()){
+                            return switch (pig.getVariant().getKey().getKey().toUpperCase()){
                                 case "COLD" -> EntityHead.PIG_COLD.getSkull();
                                 case "WARM" -> EntityHead.PIG_WARM.getSkull();
                                 case "TEMPERATE" -> EntityHead.PIG_TEMPERATE.getSkull();
